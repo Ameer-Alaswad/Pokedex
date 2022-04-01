514a3f1195f70422aaddf849318c32c3d026dfbf
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-04-01 15:04:58 +0200

    adds PokenonsList component to App and some fine tuning

--- src/components/Pokemons-list/PokemonCard.js
+++ src/components/Pokemons-list/PokemonCard.js
@@ -5,14 +5,14 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
-export default function ActionAreaCard() {
+export default function PokemonCard() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image="https://upload.wikimedia.org/wikipedia/commons/c/c9/Moon.jpg"
           alt="green iguana"
         />
         <CardContent>
---
+++ src/components/Pokemons-list/PokemonsList.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import PokemonCard from "./PokemonCard";
+
+const PokemonsList = () => {
+  return <PokemonCard />;
+};
+export default PokemonsList;
