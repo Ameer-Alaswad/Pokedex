cc0db171f8d7b999993da7898670102263ddd208
Ameer-Alaswad <ammeer.z.alaswad@gmail.com>
2022-04-03 17:36:08 +0200

    fine tuning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
 import PokemonsList from "./components/Pokemons-list/PokemonsList";
-import { QueryClient, QueryClientProvider } from "react-query";
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <PokemonsList />
-      </QueryClientProvider>
+      <PokemonsList />
     </div>
   );
 }
